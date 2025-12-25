070100c7680ee403be20e6a12e722bdaf2147b11
shybarm <Diamondscom@gmail.com>
2025-12-25 10:44:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: "client",
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "../dist/public",
     emptyOutDir: true,
   },
 });
