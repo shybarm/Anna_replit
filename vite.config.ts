f9b659c16f19cad536a946cccfa0226dbf5030c6
shybarm <Diamondscom@gmail.com>
2025-12-25 10:39:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-✗ Build failed in 110ms
-Could not resolve entry module "client/index.html".
-    at getRollupError (file:///vercel/path0/node_modules/rollup/dist/es/shared/parseAst.js:401:41)
-    at error (file:///vercel/path0/node_modules/rollup/dist/es/shared/parseAst.js:397:42)
-    at ModuleLoader.loadEntryModule (/vercel/path0/node_modules/rollup/dist/es/shared/node-entry.js:21654:20)
-    at async Promise.all (index 0) {
-  code: 'UNRESOLVED_ENTRY'
-}
-Error: Command "npm run build" exited with 1
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
+    },
+  },
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyOutDir: true,
+  },
+});
