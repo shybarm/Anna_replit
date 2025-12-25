2040df41375eb7d5fe051c24d66cd51a784ae910
shybarm <Diamondscom@gmail.com>
2025-12-25 11:06:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,21 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.join(__dirname, "client", "src"),
+      "@shared": path.join(__dirname, "shared"),
+      "@assets": path.join(__dirname, "attached_assets"),
     },
   },
-  root: "client",
+  root: path.join(__dirname, "client"),
   build: {
-    outDir: "../dist/public",
+    outDir: path.join(__dirname, "dist", "public"),
     emptyOutDir: true,
   },
 });
